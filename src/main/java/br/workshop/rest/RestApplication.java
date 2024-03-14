75df0fd7a17e2636b98d8a43305828070c415c29
Maximillian Arruda <dearrudam@gmail.com>
2024-03-13 23:31:14 -0300

    get started
    Signed-off-by: Maximillian Arruda <dearrudam@gmail.com>

---
+++ src/main/java/br/workshop/rest/RestApplication.java
@@ -0,0 +1,9 @@
+package br.workshop.rest;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class RestApplication extends Application {
+
+}
